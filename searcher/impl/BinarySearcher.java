c6039cd2493b218917284a25fcf9af31f48c7178
vitaliySoykin <ipvital19061989@gmail.com>
2023-07-02 16:09:31 +0300

    rename local variables in BinarySearcher.java

--- searcher/impl/BinarySearcher.java
+++ searcher/impl/BinarySearcher.java
@@ -5,20 +5,20 @@ import searcher.Searcher;
 public class BinarySearcher implements Searcher {
     // 1,2,3,4,5,6,7,8    3
     @Override
-    public Integer index(int[] sortedItems, int element) {
-        int start = 0;
-        int end = sortedItems.length;
-        while (start <= end) {
-            int midl = (int) ((long) start + end) / 2;
-            int midlItem = sortedItems[midl];
-            if (midlItem == element) {
-                return midl;
+    public Integer index(int[] sortedItems, int searchingItem) {
+        int right = 0;
+        int left = sortedItems.length;
+        while (right <= left) {
+            int midlIndex = (int) ((long) right + left) / 2;
+            int midlItem = sortedItems[midlIndex];
+            if (midlItem == searchingItem) {
+                return midlIndex;
             }
-            if (midlItem > element) {
-                end = midl - 1;
+            if (midlItem > searchingItem) {
+                left = midlIndex - 1;
                 continue;
             }
-            start = midl + 1;
+            right = midlIndex + 1;
         }
         return null;
     }
