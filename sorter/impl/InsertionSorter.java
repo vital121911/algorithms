c14256eb758c44808e042df48eeb91925a762ef9
vitaliySoykin <ipvital19061989@gmail.com>
2023-07-06 12:11:04 +0300

    added insertion sorting

--- sorter/impl/InsertionSorter.java
+++ sorter/impl/InsertionSorter.java
@@ -2,23 +2,17 @@ package sorter.impl;
 
 import sorter.Sorter;
 
-import java.util.Arrays;
-
 public class InsertionSorter implements Sorter {
 
     @Override
     public void sort(int[] inputArray) {
-        System.out.println("START"+ Arrays.toString(inputArray));
         for (int i = 0; i < inputArray.length; i++) {
-            int temp=inputArray[i];
-            System.out.println("I"+i);
+            int currentI = inputArray[i];
             int pt;
-            for (pt = i; ((pt>0)&&(inputArray[pt-1]>temp)); pt--) {
-                System.out.println("pt"+pt);
-                inputArray[pt]=inputArray[pt-1];
-                System.out.println(Arrays.toString(inputArray));
+            for (pt = i; ((pt > 0) && (inputArray[pt - 1] > currentI)); pt--) {
+                inputArray[pt] = inputArray[pt - 1];
             }
-            inputArray[pt]=temp;
+            inputArray[pt] = currentI;
         }
     }
 }
